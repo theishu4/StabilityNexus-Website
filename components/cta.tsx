2e2c547160e595e9db303e33cae56d0ae7ced9b2
Bruno <bruno.wp@gmail.com>
2024-08-20 13:23:51 +0300

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -19,7 +19,7 @@ export default function CallToAction() {
               className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-secondary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
             >
               <span className="relative text-lg font-semibold text-black">
-                Join us
+                Join us in Discord
               </span>
             </Link>
           </div>
